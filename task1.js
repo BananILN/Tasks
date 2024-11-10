624801c8436761b907a05ac9146cb2c5f466fbbe
BananILN <143628077+BananILN@users.noreply.github.com>
2024-11-10 16:58:44 +0400

    Update task1.js

--- task1.js
+++ task1.js
@@ -3,11 +3,49 @@
 const arr1 = [0,0,1,1,1,3,3,3,4,4,5,6,6,7,7];
 const arr2 = ['dog', 'dog', 'cat', 'cat', 'sheep', 'parrot', 'parrot'];
 
-const getUniqElement = (arr) => {
-  // ...
+const getUniqElementMap = (arr) => {
+ const LoopMap = new Map();
+
+ arr.forEach(element => {
+      LoopMap.set(element,(LoopMap.get(element) || 0 )+ 1 )
+ });
+
+
+ for(const [el , count] of LoopMap){
+  if(count === 1) { 
+    return el;
+  } 
+ }
+ return null;
 }
 
-console.log(arr1); // 5
-console.log(arr2); // 'sheep'
+
+const GetUniqElementSet= (arr) =>{
+  const seen = new Set();
+  const duble = new Set();
+
+  arr.forEach(element => {
+      if(seen.has(element)){
+        duble.add(element);
+      }else{
+        seen.add(element);
+      }
+  });
+
+  for(const el of seen){
+    if(!duble.has(el)){
+      return el;
+    }
+  }
+}
+
+
+
+
+
+
+
+console.log(GetUniqElementSet(arr1)); // 5
+console.log(getUniqElementMap(arr2)); // 'sheep'
 
 
