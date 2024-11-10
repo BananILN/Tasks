a0dfc9832e04a26eb4e3f2b0331e845a09f64048
BananILN <143628077+BananILN@users.noreply.github.com>
2024-11-10 16:59:08 +0400

    Update task2.js

--- task2.js
+++ task2.js
@@ -4,7 +4,20 @@ const arr1 = [0,0,1,1,1,1,3,3,3,4,4,5,6,6,7,7];
 const arr2 = ['dog', 'dog', 'dog', 'cat', 'cat', 'sheep', 'parrot', 'parrot'];
 
 const getPopularItems = (arr, n) => {
-  // ...
+ const Topmap = new Map();
+
+ arr.forEach(element => {
+    
+    Topmap.set(element, (Topmap.get(element)|| 0 ) +1);
+ });
+
+ const topArr = Array.from(Topmap.entries());
+
+ topArr.sort((a,b) => b[1] - a[1])
+
+ const res = topArr.slice(0,n).map(entry => entry[0])
+
+ return res;
 }
 
 console.log(getPopularItems(arr1, 2)); // [1, 3]
